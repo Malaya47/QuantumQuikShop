db58d8a208cd3d4b10946db65dd06b9b84ff71ae
Malaya Tiwari <sidzilegend@gmail.com>
2024-08-01 13:13:30 +0530

    demo comment

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import App from './App'
-import ProductsListing from './pages/ProductsListing'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';  
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from "./App";
+import ProductsListing from "./pages/ProductsListing";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// comment added
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />
-	},
-	{
-		path: '/products/:category',
-		element: <ProductsListing />
-	}
-])
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/products/:category",
+    element: <ProductsListing />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
