28b03aef8de05ea1e81457ba333001b409add7e2
Malaya Tiwari <sidzilegend@gmail.com>
2024-09-16 15:19:40 +0530

    Made social Media links working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,17 +9,36 @@ const Footer = () => {
           <h5 className="fs-3">Shoppify</h5>
           <p>Fill your wardrobes with our signature styled clothes</p>
           <div className="d-flex justify-content-start">
-            <i className="bi bi-twitter-x"></i>
-            <i className="bi bi-linkedin ms-4"></i>
-            <i className="bi bi-github ms-4"></i>
+            <a href="https://x.com" target="_blank" className="text-light">
+              <i className="bi bi-twitter-x"></i>
+            </a>
+            <a
+              href="https://linkedin.com/in/malaya-tiwari-84a951189"
+              target="_blank"
+              className="text-light"
+            >
+              <i className="bi bi-linkedin ms-4"></i>
+            </a>
+            <a
+              href="https://github.com/Malaya47"
+              target="_blank"
+              className="text-light"
+            >
+              <i className="bi bi-github ms-4"></i>
+            </a>
           </div>
         </div>
         <div className="col-md-3">
           <h5 className="fs-3">Quick Links</h5>
           <div className="ms-1">
             <Link className="text-light linkDecoration">Products</Link> <br />
-            <Link className="text-light linkDecoration">Wishlist</Link> <br />
-            <Link className="text-light linkDecoration">Cart</Link>
+            <Link to={"/wishlist"} className="text-light linkDecoration">
+              Wishlist
+            </Link>{" "}
+            <br />
+            <Link to={"/cart"} className="text-light linkDecoration">
+              Cart
+            </Link>
           </div>
         </div>
         <div className="col-md-3">
